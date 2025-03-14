cd30b064bc1b23e19ea9f20b659acb59cc9ffc76
Thibaud <creatibofficiel@gmail.com>
2025-03-14 14:31:22 +0100

    chore: update Node.js version in Dockerfile and replace bcrypt with bcryptjs in the application

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
--- src/investment/investment.module.ts
+++ src/investment/investment.module.ts
@@ -3,9 +3,10 @@ import { InvestmentService } from './investment.service';
 import { InvestmentController } from './investment.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Investment } from './entities/investment.entity';
+import { Project } from 'src/project/entities/project.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Investment])],
+  imports: [TypeOrmModule.forFeature([Investment, Project])],
   controllers: [InvestmentController],
   providers: [InvestmentService],
   exports: [InvestmentService],
